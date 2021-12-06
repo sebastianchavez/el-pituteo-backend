d0407c8a4d35a88a686fae703a297ea8b4ce284a
Sebastián <seba.chavez21@gmail.com>
2021-12-06 18:52:15 -0300

    upd https

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 require('dotenv').config()
 const app = require('./app')
 require('./config/db')
+var fs = require('fs');
+var https = require('https');
+const { PORT } = require('./config/server')
+
 
 const main = async () => {
-  await app.listen(app.get('port'))
-  console.log(`Server on port ${app.get('port')}`)
+  https.createServer({
+    cert: fs.readFileSync('mi_certificado.crt'),
+    key: fs.readFileSync('mi_certificado.key')
+  }, app).listen(PORT, function () {
+    console.log('Servidor https correindo en el puerto 443');
+  });
+
+  // await app.listen(app.get('port'))
+  // console.log(`Server on port ${app.get('port')}`)
 }
 
 main()
