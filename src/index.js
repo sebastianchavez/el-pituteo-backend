0c9b096386542a832c89df2735780ecef9e9aa88
Sebastián <seba.chavez21@gmail.com>
2021-12-06 19:32:04 -0300

    ssl habilitado

--- src/index.js
+++ src/index.js
@@ -7,16 +7,20 @@ const { PORT } = require('./config/server')
 const { AVAILABLE_SSL, HTTPS_CERT, HTTPS_KEY } = process.env
 
 const main = async () => {
-  if (AVAILABLE_SSL) {
-    https.createServer({
-      cert: fs.readFileSync(HTTPS_CERT),
-      key: fs.readFileSync(HTTPS_KEY)
-    }, app).listen(PORT, function () {
-      console.log(`Server on port ${PORT}`)
-    });
-  } else {
-    await app.listen(app.get('port'))
-    console.log(`Server on port ${app.get('port')}`)
+  try {
+    if (AVAILABLE_SSL) {
+      https.createServer({
+        cert: fs.readFileSync(HTTPS_CERT),
+        key: fs.readFileSync(HTTPS_KEY)
+      }, app).listen(PORT, function () {
+        console.log(`Server on port ${PORT}`)
+      });
+    } else {
+      await app.listen(app.get('port'))
+      console.log(`Server on port ${app.get('port')}`)
+    }
+  } catch (error) {
+    console.log('Error al levantar servidor:', error)
   }
 }
 
