883c2796e5a8475d12b7c3cc5dbda56f19d30937
Sebastián <seba.chavez21@gmail.com>
2021-12-14 02:21:29 -0300

    ip agregada

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const morgan = require('morgan')
 const { PORT } = require('./config/server')
 const routers = require('./routers')
 const { AVAILABLE_SSL } = process.env
+const expressip = require('express-ip');
 
 // settings
 if (!AVAILABLE_SSL) {
@@ -14,8 +15,13 @@ if (!AVAILABLE_SSL) {
 app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json({ limit: '50mb' })) // TODO: definir limite transferencia
+app.use(expressip().getIpInfoMiddleware);
 
 // routes
+app.use((req, res, next) => {
+    console.log('IP:', req.ipInfo)
+    next()
+})
 app.use('/api/admins', routers.adminRouter)
 app.use('/api/categories', routers.categoryRouter)
 app.use('/api/communes', routers.communeRouter)
